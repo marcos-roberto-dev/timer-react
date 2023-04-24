650c1f330c33c87d15484bba0797bf0360b5f7f2
Marcos Roberto <marcosprojetosdev@gmail.com>
2023-04-24 10:55:09 -0300

    :sparkles: definied config project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
+
 export function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <div>hello timer</div>
-    </>
-  );
+      <GlobalStyle />
+    </ThemeProvider>
+  )
 }
