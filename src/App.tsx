4b711e8ba5cdfc892041e5cf54cd7bec941ae009
Marcos Roberto <marcosprojetosdev@gmail.com>
2023-04-24 11:20:07 -0300

    :sparkles: create layout default pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
+import { Router } from './Router'
+import { Header } from './components/Header'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <div>hello timer</div>
+      <Header />
+      <Router />
       <GlobalStyle />
     </ThemeProvider>
   )
---
+++ src/Router.tsx
@@ -0,0 +1,17 @@
+import { Route, Routes, BrowserRouter } from 'react-router-dom'
+import { Home } from './pages/Home'
+import { History } from './pages/History'
+import { DefaultLayout } from './layouts/DefaultLayout'
+
+export function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<DefaultLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/history" element={<History />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
